dc425ba7e037a09c082334d545df7cbbd0491ae3
Robertarezza <roberta.rezza@gmail.com>
2024-05-08 17:51:52 +0200

    add: milestone 2

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,26 @@ export default {
    
     };
   },
-
+  methods: {
+    // Metodo per ottenere l'icona della bandiera corrispondente alla lingua
+    getLanguageFlagIcon(language) {
+      if (!language) return "";
+      switch (language.toLowerCase()) {
+        case 'it':
+        console.log("Caricata immagine per lingua italiana");
+        return new URL(`../assets/img/it.jpg`, import.meta.url).href;
+        case 'en':
+          return new URL(`../assets/img/en.jpg`, import.meta.url).href;
+        case 'es':
+          return new URL(`../assets/img/es.jpg`, import.meta.url).href;
+          case 'gl':
+          return new URL(`../assets/img/gl.jpg`, import.meta.url).href;
+        default:
+          return new URL(`../assets/img/mondo.jpg`, import.meta.url).href;
+      }
+    },
+  },
+  
  
  
 };
@@ -23,10 +42,12 @@ export default {
   <div class="container mt-5">
     <div class="row row-cols-4">
       <div class="col mb-4" v-for="movie in store.movieArrey"  >
-        <h1>Titolo: {{movie.title}}</h1>
-        <h2>Titolo Originale: {{movie.original_title}}</h2>
-        <h3>Lingua Orgiginale:{{movie.original_language}}</h3>
+      <div class="card">
+        <h1> {{movie.title}}</h1>
+        <h2> {{movie.original_title}} </h2>
+        <img :src="getLanguageFlagIcon(movie.original_language)" alt="">
         <h4>Voto: {{movie.vote_average}}</h4>
+      </div>
       </div>
     </div>
   </div>
@@ -34,5 +55,8 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+img{
+  width: 25%;
+}
 
 </style>
